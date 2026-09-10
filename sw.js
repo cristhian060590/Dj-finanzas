15d72e6ef4ebc86918a62a8302b183cd4811e5f5
cristhian060590 <cristhian060590@gmail.com>
2026-09-10 18:44:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dj-finanzas-v5-4-2026-09-09';
+const CACHE='dj-finanzas-v6-liquid-glass-new-icon-2026-09-10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
