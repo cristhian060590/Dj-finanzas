d191962a2c626c80271a9e89284575c909ba662d
cristhian060590 <cristhian060590@gmail.com>
2026-09-10 19:21:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dj-finanzas-v6-2-filtros-parpadeo-2026-09-10';
+const CACHE='dj-finanzas-v6-2-1-corrected-2026-09-10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
